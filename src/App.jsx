85ed8242c3d11159e903a6fc8a2a280a60ec7401
Yug Marwaha <yugmarwaha987@gmail.com>
2025-11-09 21:11:41 -0600

    Update layout: timer left, todo list center

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { Container, Row, Col } from 'react-bootstrap'
-import Timer from './components/Timer'
-import TodoList from './components/TodoList'
+import { Container, Row, Col } from "react-bootstrap";
+import Timer from "./components/Timer";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 function App() {
   return (
-    <Container fluid className="min-vh-100 bg-light py-4">
+    <Container fluid className="min-vh-100 py-5">
       <Container>
-        <div className="text-center mb-5 p-4 bg-primary text-white rounded shadow">
-          <h1 className="display-4 fw-bold">📝 Todo Timer App</h1>
-          <p className="lead mb-0">Manage your tasks with timers</p>
+        <div className="text-center mb-5 p-5 text-white header-gradient">
+          <h1 className="display-3 fw-bold mb-3">
+            <span style={{ fontSize: "3rem" }}>⏰</span> Todo Timer App
+          </h1>
+          <p
+            className="lead mb-0"
+            style={{ fontSize: "1.2rem", opacity: 0.95 }}
+          >
+            Boost your productivity with focused time management
+          </p>
         </div>
-        
-        <Row className="g-4">
-          <Col md={6}>
+
+        <Row className="g-4 justify-content-center">
+          <Col lg={3} md={4} sm={12} className="order-1">
             <Timer />
           </Col>
-          <Col md={6}>
+          <Col lg={6} md={8} sm={12} className="order-2">
             <TodoList />
           </Col>
         </Row>
       </Container>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
