e04936b3e672f7cbbcce911942063da4f1c4508c
Yug Marwaha <yugmarwaha987@gmail.com>
2026-02-04 20:21:09 -0600

    Migrate TodoPage.jsx from react-bootstrap to plain CSS

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,21 +1,18 @@
-import { Container, Row, Col } from "react-bootstrap";
 import TodoList from "../components/TodoList";
 
 function TodoPage() {
   return (
     <div className="page-wrapper">
-      <Container>
+      <div className="container">
         <div className="page-header">
           <h1>Task Manager</h1>
           <p>Organize your tasks and stay productive.</p>
         </div>
 
-        <Row className="justify-content-center">
-          <Col lg={8} md={10}>
-            <TodoList />
-          </Col>
-        </Row>
-      </Container>
+        <div style={{ maxWidth: 720, margin: "0 auto" }}>
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
