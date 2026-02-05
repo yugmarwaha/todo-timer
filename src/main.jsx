066d39a4af28e1b5c0a034cea3c361b0ead84058
Yug Marwaha <yugmarwaha987@gmail.com>
2026-02-04 20:19:13 -0600

    Remove Bootstrap CSS import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
